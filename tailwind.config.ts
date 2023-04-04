a203b877a16340f422b72ece123fccefb9b75c53
Paul <pmarik@uwalumni.com>
2023-04-03 22:34:22 -0500

    feat: added svgr, initial images on home page and mobile section scaffolding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-teal': '#06BD9A',
+      },
+      backgroundImage: {
+        'main-pattern': "url('/images/main-pattern.jpg')",
+        'pattern-2': "url('/images/pattern-2.jpg')",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
