1269905fec90412c723158241dcd7c38984f7205
Paul <pmarik@uwalumni.com>
2023-04-27 14:22:49 -0500

    feat: adds sign-up icons and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,14 @@ import IconFlag from '../images/svgs/icon-flag.svg';
 import PhoneView from '../images/phone-app-view.jpg'
 import IconLogo from '~/components/atoms/IconLogo'
 import Button from '~/components/atoms/Button'
+import IconLineUp from '~/images/svgs/icon-line-up.svg'
+import IconWrite from '~/images/svgs/icon-write.svg'
+import IconRadar from '~/images/svgs/icon-radar.svg'
+import IconCrown from '~/images/svgs/icon-crown.svg'
+import IconBlocks from '~/images/svgs/icon-blocks.svg'
+import IconDiamond from '~/images/svgs/icon-diamond.svg'
+import IconPlay from '~/images/svgs/icon-play.svg'
+import IconLogoWhite from '~/images/svgs/icon-logo-white.svg'
 
 
 
@@ -91,14 +99,14 @@ const Home: NextPage = () => {
       </header>
 
       <main className="-mt-20 flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-[#FFFFFF] to-[var(--gradient-color)]">
-        <div className=" flex flex-col items-center justify-center pb-12 w-full">
+        <div className=" flex flex-col items-center justify-center w-full">
           <div className="container flex flex-col items-center justify-center mt-40 md:flex-row md:h-screen md:mt-0 md:justify-evenly ">
             <div className="space-y-5 max-w-screen-sm">
               <h1 className="text-4xl font-extrabold tracking-tight text-center text-slate-950 lg:text-[5rem] md:text-left md:leading-none drop-shadow-md">
                 Track & improve your trades
               </h1>
               <p className="text-center md:text-left md:max-w-prose">Gain consistency through data. View insights and performance of your trades over time.</p>
-              <Button to="https://create.t3.gg/en/introduction">Explore Now →</Button>
+              <Button to="#features">Explore Now →</Button>
             </div>
             <Image 
               src={HeroImage}
@@ -214,29 +222,134 @@ const Home: NextPage = () => {
               <div className="space-y-5">
                   <h2 className="text-4xl md:text-5xl font-bold text-slate-950 drop-shadow-md">Ready to get started?</h2>
                   <p className="lg:max-w-prose">Practice makes perfect, and Trade Tracker improves your steady progress with intuitive features.</p>
-                  <Button to="https://create.t3.gg/en/introduction">Start free trail →</Button>
               </div>
 
-              <div className="flex justify-around w-3/5">
-                <div className="bg-slate-50 w-5/12 p-6 rounded-lg">
+              <div className="flex justify-around w-4/5">
+                <div className="bg-slate-50 w-5/12 p-6 rounded-lg h-min-content">
                   <h3>Free</h3>
-                  <p>1 portfolio</p>
+                  <ul className="my-4 space-y-3">
+                    <li>
+                      <IconLineUp className="inline-block" width="2rem" height="2rem"/>
+                      <p className="inline-block ml-3">1 portfolio</p>
+                    </li>
+                    <li>
+                      <IconWrite className="inline-block" width="2rem" height="2rem"/>
+                      <p className="inline-block ml-3">40 trades a month</p>
+                    </li>
+                    <li>
+                      <IconRadar className="inline-block" width="2rem" height="2rem"/>
+                      <p className="inline-block ml-3">Watchlist alerts</p>
+                    </li>
+                  </ul>
+                  <Button to="https://create.t3.gg/en/introduction">Sign up</Button>
+
                 </div>
-                <div className="bg-slate-50 w-5/12 p-6 rounded-lg">
+                <div className="custom-gradient w-5/12 p-6 rounded-lg h-min-content">
                   <h3>Pro</h3>
-                  <p>All portfolios</p>
-                  <p>AI journal suggestions</p>
+                  <ul className="my-4 space-y-3">
+                    <li>
+                      <IconCrown className="inline-block" width="2rem" height="2rem"/> 
+                      <p className="inline-block ml-3"> All portfolios</p>
+                    </li>
+                    <li>
+                      <IconBlocks className="inline-block" width="2rem" height="2rem"/> 
+                      <p className="inline-block ml-3">Unlimited Trades</p>
+                    </li>
+                    <li>
+                      <IconDiamond className="inline-block" width="2rem" height="2rem"/> 
+                      <p className="inline-block ml-3">AI journal suggestions</p>
+                    </li>
+                    <li>
+                      <IconPlay className="inline-block" width="2rem" height="2rem"/> 
+                      <p className="inline-block ml-3">First for features & updates</p>
+                    </li>
+                  </ul>
+                  <Button to="https://create.t3.gg/en/introduction">Try for free</Button>
+
                 </div>
               </div>
             </div>
           </div>
 
+          <div>
+            <form className="px-8 pt-8 pb-12 mb-4">
+              <div className="mb-4">
+                <h4 className="text-center">Woah, you made it all the way down here. <br/> Reward yourself by signing up to our awesome newsletter.</h4>
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+                  {/* Email */}
+                </label>
+                <input className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+              </div>
+            </form>
+          </div>
 
-          <footer className="bg-[var(--dark-1)] w-full my-32">
-              <p>footer</p>
+          <a href="#" className="bg-color-600 w-full text-center p-6 mt-16 text-slate-50 hover:bg-color-500">Back to top</a>
+
+          <footer className="bg-[var(--dark-1)] w-full ">
+          <nav
+            className="
+              flex flex-wrap
+              items-center
+              justify-between
+              w-full
+              px-16
+              text-lg text-gray-700
+              relative
+            "
+          >
+            <div className="my-16">
+              <IconLogoWhite width="12rem" height="5rem" />
+              <p className="text-slate-300">© Trade Tracker 2023</p>
+            </div>
+
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="menu-button"
+                className="h-6 w-6 cursor-pointer md:hidden block"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          
+            <div className="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+                <ul
+                  className="
+                    pt-4
+                    text-base text-gray-700
+                    md:flex
+                    items-center
+                    md:justify-between 
+                    md:pt-0"
+                >
+                  <li className="p-4">
+                    <a className=" block text-slate-50 hover:text-main-color-400 transition-colors" href="#features">Features</a>
+                  </li>
+                  <li className="p-4">
+                    <a className=" block text-slate-50 hover:text-main-color-400 transition-colors" href="#">Pricing</a>
+                  </li>
+                  <li className="p-4">
+                    <a
+                      className="md:px-6 md:py-2 block hover:bg-main-color-400 text-slate-50 bg-[var(--dark-1)] rounded-2xl transition-colors"
+                      href="#">Sign Up</a>
+                  </li>
+                  <li className="p-4">
+                    <a
+                      className=" block text-slate-50 hover:text-main-color-400 text-[var(--dark-1)] transition-colors"
+                      href="#">Log In</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
           </footer>
           
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-slate-950 hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
@@ -259,13 +372,15 @@ const Home: NextPage = () => {
                 to deploy it.
               </div>
             </Link>
-          </div>
+          </div> */}
+{/* 
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-slate-950">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
-          </div>
+          </div> */}
+          
         </div>
       </main>
     </>
