02b21a9ae41c93fe3cd747b2e986837c070ad974
Paul <pmarik@uwalumni.com>
2023-11-15 17:26:52 -0600

    feat: coming soon form, and redirects on web app - to be removed

--- apps/web/src/components/molecules/Forms/RegisterForm/RegisterForm.tsx
+++ apps/web/src/components/molecules/Forms/RegisterForm/RegisterForm.tsx
@@ -105,7 +105,7 @@ export default function RegisterForm(){
                         type='password'
                         aria-label="Enter Password"
                      />
-                    <p><CloseCircle className="inline mb-[4px]"/> {errors.password?.message}</p>
+                    {errors.password?.message && <p><CloseCircle className="inline mb-[4px]"/> {errors.password?.message}</p>}
 
                 </div>
 
--- apps/web/src/components/organisms/ComingSoon/ComingSoon.tsx
+++ apps/web/src/components/organisms/ComingSoon/ComingSoon.tsx
@@ -1,10 +1,10 @@
-import IconLogo from '~/components/atoms/IconLogo'
+"use client"
+
 import ButtonLink from '~/components/atoms/ButtonLink'
 import { useForm, ValidationError } from "@formspree/react";
 import { Button } from '~/components/ui/Button'
 import { Input } from '~/components/ui/Input'
 import { Label } from '~/components/ui/Label'
-
 
 export default function ComingSoon(){
 
@@ -12,33 +12,27 @@ export default function ComingSoon(){
 
     return(
                
-                <div >
-
+                <>
                     {state.succeeded ?
-                         <section className="mx-4 my-6">
+                         <section className="w-3/4">
                             <h1 className="text-4xl mb-4 text-center font-bold">Thank you!</h1>
                             <p className="text-center mb-4">Trade Tracker is currently under development. <br/>We will email you for early access once we launch.</p>
                             <p className="text-center mb-12">Check back soon.</p>
-
-                            <ButtonLink to="/" display="secondary" variant={'main'} >Exit →</ButtonLink>
-
-                            <IconLogo className="relative mt-16 pl-4 translate-x-[-25%] left-1/2"/>
                          </section>
                     :
-                        <section className="mx-4 my-6 ">
-                            <h1 className="text-4xl mb-4 text-center font-extrabold">Sign Up</h1>
+                        <section className="w-3/4">
+                            <h1 className="text-4xl mb-4 text-center font-bold">Sign Up</h1>
                             <p className="text-center mb-4">It&apos;s free!</p>
-
-                            <form onSubmit={(event) => void handleSubmit(event)} className="space-y-5 mt-4 mb-8">
+                            <form onSubmit={(event) => void handleSubmit(event)} className='space-y-5 mt-4 mb-8'>
                                 <div className="grid w-full items-center gap-1.5">
-                                        <Label htmlFor="name">Name</Label>
-                                        <Input
-                                            required
-                                            id="name"
-                                            aria-label="Enter Name"
-                                            name="name"
-                                        />
-                                        <ValidationError prefix="Name" field="name" errors={state.errors} />
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input
+                                        required
+                                        id="name"
+                                        aria-label="Enter Name"
+                                        name="name"
+                                    />
+                                    <ValidationError prefix="Name" field="name" errors={state.errors} />
                                 </div>
 
                                 <div className="grid w-full items-center gap-1.5 pb-4">
@@ -55,13 +49,9 @@ export default function ComingSoon(){
 
                                 <Button type="submit" disabled={state.submitting} className='w-full' variant="default" size="lg">Submit →</Button>
                                 <ValidationError errors={state.errors} />
-                                <ValidationError errors={state.errors} />
                             </form>
-
-                            <IconLogo className="relative mt-10 pl-4 translate-x-[-25%] left-1/2"/>
-
                         </section>
                     }
-                </div>
+                </>
     )
 }
